769de9cb2b094d910e640583a138a0e694978802
Grant Wu <grant.wu@petuum.com>
2018-11-20 18:54:16 -0500

    Add additional test case, demonstrating why a possible workaround doesn't suffice

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { makeExecutableSchema, mergeSchemas } from "graphql-tools";
 const item = makeExecutableSchema({
   typeDefs: `
 type Item {
-  name: String
+  name: Int
 }`});
 
 const extendingSchemaGQLTag = gql`
@@ -12,4 +12,30 @@ type Query {
   findItemCostMoreThan(giveCost: Float): [Item]
 }`;
 
+// This works because makeExecutableSchema defines (in the returned GraphQLSchema) all basic types used.
+const extendingSchemaGQLMES = makeExecutableSchema({
+  typeDefs: `
+type Query {
+  findItemCostMoreThan(giveCost: Float): [Float]
+}`});
+
+// This works...
+mergeSchemas({ schemas: [item, extendingSchemaGQLMES] });
+
+
+console.log();
+console.log("This doesn't work because makeExecutableSchema seems to require " +
+  "a self-contained schema - this refers to Item, which isn't defined here");
+try {
+  makeExecutableSchema({
+    typeDefs: `
+  type Query {
+    findItemCostMoreThan(giveCost: Float): [Item]
+  }`});
+} catch (err) {
+  console.error(err);
+}
+
+console.log();
+console.log("This doesn't work, because the gql tag doesn't seem to properly define the Float type");
 mergeSchemas({ schemas: [item, extendingSchemaGQLTag] });
